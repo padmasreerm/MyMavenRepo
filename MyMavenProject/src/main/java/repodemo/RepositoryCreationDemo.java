416d4f94d137c863bc67441e0fca1b19fa9b4716
MYPC <MYPC@DESKTOP-VA9RJV7>
2024-08-23 08:14:19 +0530

    second

---
+++ MyMavenProject/src/main/java/repodemo/RepositoryCreationDemo.java
@@ -0,0 +1,15 @@
+package repodemo;
+
+public class RepositoryCreationDemo {
+	public static int display(int a,int b)
+	{
+		int c=a+b;
+		return c;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int value=RepositoryCreationDemo.display(10,15);
+System.out.println("The sum is"+value);
+	}
+
+}
